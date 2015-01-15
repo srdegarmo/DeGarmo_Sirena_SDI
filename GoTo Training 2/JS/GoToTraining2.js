aeffbb436c977f6020114ee90848e5f6327939ea
srdegarmo <srdegarmo@fullsail.edu>
2015-01-14 23:38:31 -0600

    Prompt & Alert
    Prompted user for name
    Used an alert to greet user by name

--- GoTo Training 2/JS/GoToTraining2.js
+++ GoTo Training 2/JS/GoToTraining2.js
@@ -5,3 +5,14 @@ GoTo Training Day #2
 1/14/2015
  */
 //alert("Testing to see if the JS file is attached to the html.");
+
+//Create an age calculator
+
+//Ask the user their name
+var name=prompt("Please type in your name");
+
+//console.log out the name
+console.log(name);
+
+//Welcome the user with an alert
+alert("Welcome "+name+"! Let's figure out your age");
