af680175494bd4df89c95bc2d8db27f99f3cb605
srdegarmo <srdegarmo@fullsail.edu>
2015-01-15 22:31:45 -0600

    Array Screencast
    WK 2 Array screencast

--- Associating JS/JS/script.js
+++ Associating JS/JS/script.js
@@ -1,11 +1,2 @@
-//Variables of different types: numbers, boolean, and strings
-
-var age = 20; //number variable
-
-var isStudent = false; //Boolean variable
-
-var firstName = "Sirena"; //String variable
-
-
-//Array
-var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
+var familyNames=["Sirena", "Morgie", "Dillan"];
+console.log(familyNames[0]);
