d669d0ded666908952286d393b9fb4acbb814a32
srdegarmo <srdegarmo@fullsail.edu>
2015-01-15 21:09:51 -0600

    Age Calculator
    GoTo Training 2 age calculator in JS

--- GoTo Training 2/JS/GoToTraining2.js
+++ GoTo Training 2/JS/GoToTraining2.js
@@ -31,3 +31,21 @@ var age=currentYear-yearBorn-1;
 
 console.log(name+" you are "+age+" years old.");
 alert(name+" you are "+age+" years old.");
+
+//Make it MORE complicated
+
+//Ask the user how many years in the future they would like to know their age.
+var yearsMore=prompt("How many years in the future would you like to know your age?");
+console.log(yearsMore);
+
+//Calculate future age
+//+plus sign does double duty: addition & concatenation
+//prompts ONLY return text strings!!!!
+var futureAge=age+yearsMore;
+console.log(futureAge);
+
+var a=Number("40");
+console.log(a);
+
+var b=Number("40 years old");
+console.log(b);
