9df2ebd92ae80ed4d2bd03a201c7148223df3418
srdegarmo <srdegarmo@fullsail.edu>
2015-01-15 22:15:13 -0600

    Values & Variables
    Following along with Array screencast

--- Associating JS/JS/script.js
+++ Associating JS/JS/script.js
@@ -1,3 +1,11 @@
-//JavaScript
-var age;
-age=20;
+//Variables of different types: numbers, boolean, and strings
+
+var age = 20; //number variable
+
+var isStudent = false; //Boolean variable
+
+var firstName = "Sirena"; //String variable
+
+
+//Array
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"];
