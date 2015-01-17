f48a512632e5c337478c1b2b21dbb608396ddeac
srdegarmo <srdegarmo@fullsail.edu>
2015-01-17 17:23:18 -0600

    WK2 Add Modulo Operator
    Edit existing expressions to incorporate modulo operator so computer is
    able to determine how many slices of pizza will remain for Sparky & how
    many whole pieces of pizza each guest will get if the pizza is split
    evenly

--- Expressions_Worksheet/JS/script.js
+++ Expressions_Worksheet/JS/script.js
@@ -7,7 +7,7 @@ Expressions Worksheet
  */
 //alert("Testing to see if the JS file is attached to the html.");
 
-
+//DOG YEARS
  //Dogs age 7 times faster than humans ( 1 human year = 7 dog years)
  //Calculate Sparky the pit bulls age in dog years based on his actual age
  //Ask user to provide Sparky's age in human years
@@ -21,7 +21,7 @@ Expressions Worksheet
  //Notify user of Sparky's age in human and dog years using an alert
  alert("Sparky is " + humanYears + " human years old; which is " + dogYears + " in dog years.");
 
-
+//SLICE OF PIE, pt. 1
  //Create expression to calculate how much pizza each party-goer can eat
   //all pizzas have the same number of identically sized slices
  //Tell user what we are calculating
@@ -40,11 +40,17 @@ Expressions Worksheet
  var totalSlices = slices * pizza;
  //print total number of slices to console
  console.log(totalSlices);
-//Divide total number of slices by people to find out how many slices each person gets
- var slicesPerPerson = totalSlices / people;
- //Print result to console
- console.log(slicesPerPerson);
+//Determine how many slices will be leftover if each guests gets whole slices using the modulo operator
+ var leftoverSlices = totalSlices % people;
+ //Subtract leftover slices from total slices to determine how many whole slices are available to guests
+ var availSlices = totalSlices - leftoverSlices;
+ //Divide available slices by the number of people to find out how many whole slices each person can eat if everyone gets the same amount
+ var slicesPerPerson = availSlices / people;
  //Show user how many pieces of pizza each person at the party ate using an alert
  alert("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
- 
 
+//SLICE OF PIE, pt. 2
+ //Sparky gets to eat the leftover pizza after all the whole slices are divided up between the guests
+ //Determine how many slices of pizza will be leftover for Sparky using modulo operator
+
+ var sparkySlices = alert("Let's determine how many pieces of pizza will be leftover for Sparky after each party guest gets " + slicesPerPerson + " pieces of pizza.");
