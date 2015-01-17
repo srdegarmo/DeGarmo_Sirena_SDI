fa7784d93b30684bdffaba41056407b976482cda
srdegarmo <srdegarmo@fullsail.edu>
2015-01-16 22:14:50 -0600

    Prom & Alert
    Prompted user for name & used an alert to say hello

--- DeGarmo_Sirena_Expressions_Assignment/JS/script.js
+++ DeGarmo_Sirena_Expressions_Assignment/JS/script.js
@@ -5,4 +5,16 @@ SDI Section #3
 Expressions Assignment
 1/16/2015
  */
-alert("Testing to see if the JS file is attached to the html.");
+//alert("Testing to see if the JS file is attached to the html.");
+
+//Creating calculator to determine how long it will take user to save enough money to buy a particular item
+
+//Ask user their name
+ var name = prompt("Please enter your name:");
+//console.log out name
+console.log(name);
+
+//Welcome user with an alert
+ alert("Hello, " + name + "! Let's figure out how long it will take you to save enough money to purchase a certain item");
+
+ 
