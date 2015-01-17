4240c9fe54c3475fc315187c467665af2ce6af24
srdegarmo <srdegarmo@fullsail.edu>
2015-01-17 13:52:40 -0600

    WK2 Basic Expressions
    Following along with basic expressions screencast

---
+++ Expressions_Screencasts/JS/script.js
@@ -0,0 +1,22 @@
+
+ /*
+Sirena DeGarmo
+SDI Section #3
+Expressions Screencasts
+1/16/2015
+ */
+alert("Testing to see if the JS file is attached to the html.");
+
+ //BASIC EXPRESSIONS
+
+ var a = 2; //sets up variable & defines it with 2
+ a = a + 3; //takes value of a+3 and stores it in a
+ console.log(a); //if we want to add to a and change a, we use an assignment operator
+
+var a = 2; //sets up variable & defines it
+var b;
+b = a + 3; //store value of a+3 inside b
+console.log(b); //will show you value of a+3
+console.log(a); //will show you value of a
+
+
