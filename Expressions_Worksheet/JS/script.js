328038399e2b41ba3e07e6c8bbca2d15e8c4f27c
srdegarmo <srdegarmo@fullsail.edu>
2015-01-17 16:54:37 -0600

    Sparky's age
    Used givens, constants, variables, and expressions to determine
    Sparky’s age in dog years based on his age in human years. Displayed
    results to user using an alert.

---
+++ Expressions_Worksheet/JS/script.js
@@ -0,0 +1,33 @@
+
+ /*
+Sirena DeGarmo
+SDI Section #3
+Expressions Worksheet
+1/14/2015
+ */
+//alert("Testing to see if the JS file is attached to the html.");
+
+
+ //Dogs age 7 times faster than humans ( 1 human year = 7 dog years)
+ //Calculate Sparky the pit bulls age in dog years based on his actual age
+ //Ask user to provide Sparky's age in human years
+ var humanYears = prompt("Let's find out how old Sparky is in dog years. \nFirst, please enter his age in human years:");
+ //To calculate how old Sparky is in human years we have to multiply humanYears by 7
+ var yearDiff = humanYears * 7;
+ //Create variable to catch answer
+ var dogYears = yearDiff;
+ //Print product of humanYears and dogYears to console
+ console.log(dogYears);
+ //Notify user of Sparky's age in human and dog years using an alert
+ alert("Sparky is " + humanYears + " human years old; which is " + dogYears + " in dog years.");
+
+
+ //Create expression to calculate how much pizza each party-goer can eat
+  //assume all pizzas have the same number of identically sized slices
+ var slices = alert("Let's determine how much pizza each person that comes to the party can eat.");
+ var slices = prompt("How many slices are in each pizza?");
+ var people = prompt("How many people are coming to the party?");
+ var pizza = prompt("How many pizzas were ordered?");
+ console.log(slices);
+ console.log(people);
+ console.log(pizza);
