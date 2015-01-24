0cdf2c15d9b6666b78579935f61e7239fca68734
srdegarmo <srdegarmo@fullsail.edu>
2015-01-24 12:58:01 -0600

    If & else
    Add if & else statement

--- Practice/Conditionals Screencasts/JS/script.js
+++ Practice/Conditionals Screencasts/JS/script.js
@@ -7,15 +7,16 @@
  */
 //alert("Testing to see if the JS file is attached to the html.");
 
-
-//Print code if child is old enough to ride roller coaster
-var kidHeight = 50;
+//identify variables
+var kidHeight = 60;
 var minHeight = 48;
 
-//if child is old enough print to the console "you can ride!"
-//if the kid is over 48in tall
-if(kidHeight > minHeight){
- //if oldEnough is true, computer performs code in curly braces
- console.log("You can ride the coaster!");
+//if child is tall enough, print "you can ride" to console
+if(kidHeight >= minHeight){
+ //code to perform if variable is true
+ console.log("You can ride the coaster!")
+}else{
+ console.log("Sorry, you are not tall enough to ride the coaster.")
 }
+
 
