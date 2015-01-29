9131a6487c69d7f5479fef585fcc58e2c930b42c
srdegarmo <srdegarmo@fullsail.edu>
2015-01-28 19:28:48 -0600

    Beginning JS
    Following along with Beginning JavaScript text book

---
+++ Practice/Beginnning Javascript/JS/script.js
@@ -0,0 +1,17 @@
+
+ /*
+Sirena DeGarmo
+SDI Section #3
+GoTo Training Day #2
+1/14/2015
+
+alert("Testing to see if the JS file is attached to the html.");
+*/
+
+ alert("Here's my JS document");
+
+ /*
+ Integers range from -2^53 to 2^53
+ Fractions known as floating point numbers
+ String is one or more characters of text
+  */
