091f17131b4b0f9d13cb9c556694defbe873c298
srdegarmo <srdegarmo@fullsail.edu>
2015-01-30 10:04:28 -0600

    Experimenting with variable scope
    Defining multiple variables with the same name & gaining understanding
    of how the computer will choose which variable to use

--- Practice/Functions_Screencast/JS/script.js
+++ Practice/Functions_Screencast/JS/script.js
@@ -2,10 +2,11 @@
  /*
 Sirena DeGarmo
 SDI Section #3
-GoTo Training Day #2
-1/14/2015
+Functions Screencast
+1/28/2015
  */
-alert("Testing to see if the JS file is attached to the html.");
+
+//alert("Testing to see if the JS file is attached to the html.");
 
  /*
  function functionName(parameters){
@@ -14,10 +15,24 @@ alert("Testing to see if the JS file is attached to the html.");
 
   functions have to be called/invoked
   functionName();
+
+  Variable Scope
+  when calling a function it's going to use the variable inside of it's function - variables are mostly contained within their functions
   */
 
  function yourName(){
   console.log("Sirena.");
  }
-
  yourName();
+
+ var number1 = 15;
+
+ function addNumbers(){
+  var number1 = 10;
+  var number2 = 20;
+  var number3 = 30;
+  var number4 = 40;
+  var sum = number1 + number2 + number3 + number4;
+  console.log(sum);
+ }
+ addNumbers();
