d89de0e316fd984136f54a8ffd24e1d85c863f31
srdegarmo <srdegarmo@fullsail.edu>
2015-01-30 15:44:59 -0600

    Area of rectangle - function
    Define & call area of rectangle function

--- Functions_worksheet/JS/script.js
+++ Functions_worksheet/JS/script.js
@@ -5,4 +5,14 @@ SDI Section #3
 Functions worksheet
 1/28/2015
  */
-alert("Testing to see if the JS file is attached to the html.");
+//alert("Testing to see if the JS file is attached to the html.");
+
+ //EXAMPLE - calculate area of a rectangle
+ var width = 5;
+ var height = 6;
+ var area = calculateArea(width, height);
+ console.log("The area of the rectangle is " + area);
+
+ function calculateArea(w, h){
+  return w*h;
+ }
