c9aff4e29aa89c74f6f225a7085ac7c593ddddca
srdegarmo <srdegarmo@fullsail.edu>
2015-01-31 10:10:33 -0600

    Validate prompts A & B using loop
    Validate user prompt for A & B using a loop

--- DeGarmo_Sirena_Functions_Assignment/JS/script.js
+++ DeGarmo_Sirena_Functions_Assignment/JS/script.js
@@ -7,13 +7,10 @@ Functions Assignment
 */
 //alert("Testing to see if the JS file is attached to the html.");
 
- //1 normal function - w/ argument and parameter
  //1 anonymous function - w/ argument and parameter
  //1 function needs 3 parameters
  //value returned for each function
- //no main code variable inside of function - must pass in as argument
  //all user prompts must be validated using a while loop
-
 
 
  //SOlVE PYTHAGORAS EQUATION
@@ -28,17 +25,26 @@ Functions Assignment
  var a = prompt("What is the value of a:");
  //Ask user for B
  var b = prompt("What is the value of b:");
- //Ask user for C
- var c = prompt("What is the value of c:");
 
 //invoke function & assign variable to hold the info the function returns
- var pythagoras = calcTotal(a, b, c);
+ var pythagoras = calcTotal(a, b);
 
  //use function with parameters to calculate answer & return results to function invocation
- function calcTotal(a, b, c){
+ function calcTotal(a, b){
      var results = a*a + b*b;
      return results;
  }
 //print results to console
  console.log("The answer to the equation is " + pythagoras + " = " + "c^2");
- 
+
+ //validate user prompt for A using a loop
+ while(a >= 15){
+     a--;
+     console.log(a);
+ }
+
+ //validate user prompt for B using a loop
+ while(b > 0){
+     b--;
+     console.log(b);
+ }
