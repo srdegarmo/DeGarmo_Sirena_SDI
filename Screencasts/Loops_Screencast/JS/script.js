f79e77f469690856052851c7ea0148df76fbdedc
srdegarmo <srdegarmo@fullsail.edu>
2015-01-30 19:50:07 -0600

    For & Do While loops
    For & Do While loops

--- Screencasts/Loops_Screencast/JS/script.js
+++ Screencasts/Loops_Screencast/JS/script.js
@@ -12,13 +12,8 @@ alert("Testing to see if the JS file is attached to the html.");
  //Apply loops
  //Understand difference between types of loops
 
- /*Regular conditional
- var b = 10;
- if ( b > 0){
-  console.log(b);
- }
- */
-
+ //While Loop
+ console.log("~~~~~LOOPS~~~~~");
  //Make regular "if" statement a loop by substituting "if" with "while"
  var b = 10; //sets up index
  //says while b is greater than 0, run this code
@@ -30,3 +25,21 @@ alert("Testing to see if the JS file is attached to the html.");
   b--; //increments or decrements index (either counts up from variable or down from variable)
  }
 //loop COUNTS DOWN from variable starting point! EX: 10 kegs on the wall, 9 kegs on the wall, etc.
+
+ //Do While Loop
+ //Only perform loop if "while" condition is true
+console.log("~~~~~DO WHILE LOOP~~~~~~");
+ var c = 10;
+ do{
+  console.log(c + " kegs on the wall.");
+  c--;
+ }
+ while(c>0);
+
+ //For Loop
+ console.log("~~~~~FOR LOOP~~~~~");
+ //combination of all loops put together
+ for (var i = 10; i > 0; i--){
+  console.log(i + " kegs on the wall");
+ }
+
